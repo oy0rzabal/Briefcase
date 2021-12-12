a4942bccc1924b1b60e6e2b20aec121fa5d725aa
oy0rzabal <oy0rzabal.l0pez@gmail.com>
2021-12-12 13:16:36 -0600

    Actulizacion

---
+++ Notas.py
@@ -0,0 +1,24 @@
+#Instalacion y Activacion de Cookiecutter
+mkdir "Creamos la carpeta donde queremos instalar Cookiecutter"
+
+cd #Ingresamos a la carpeta creada anteriormente
+
+conda config --add channels conda-forg #Configurar para que se vaya directamente la intalacion a esa carpeta y a conda global
+
+#Crear ambiente
+conda create --name "Nombre de la carpeta" cookiecutter=1.7.3
+
+#Ir al ambiente
+conda activate "Nombre de la carpeta"
+#o salir del ambiente
+conda descativate "Nombre de la carpeta"
+#Exportar el ambiente
+conda env export --from-history --file environment.yaml#Vamos hacer que los pasos anteriormente echos se pasen a ese archivo llamado "enviromment.yaml"
+
+#Ver el archivo 
+cat environment.yaml
+
+#Crear la plantilla con cookiecutter
+cookiecutter https://github.com/platzi/curso-entorno-avanzado-ds --checkout cookiecutter-personal-platzi
+
+#Se puede ir directo al git para mas informacion sobre la cracion de plantillas
